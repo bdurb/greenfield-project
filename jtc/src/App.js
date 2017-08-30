58cb3a2d86f734bf9928b360a618ed11e4ed107d
bdurb <brian@rpmgreenville.com>
2017-08-30 15:28:20 -0400

    added image

--- jtc/src/App.js
+++ jtc/src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Conclusion from './conclusions';
+import brain from './img/brain.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h1>Struggling With a New Idea?</h1>
-          <h3 className='App-logo'>Don't Bother that Pesky Brain</h3>
+          <h1>Don't know what to do?</h1>
+          <img className="App-logo" src ={brain}/>
+          <h2 className="catch">Don't Bother that Pesky Brain!</h2>
         </div>
         <Conclusion />
       </div>
